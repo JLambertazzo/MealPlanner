ed919b4c401999869175ccc4e7e76f281e4a32f1
Julien <jbertazzolambert@gmail.com>
2020-12-22 10:28:24 -0600

    fixed calendar date selection thingy

--- client/src/react-components/Calendar/index.js
+++ client/src/react-components/Calendar/index.js
@@ -6,11 +6,38 @@ import './style.css'
 import NavBar from '../NavBar'
 
 export class CalendarView extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      datesWithMeals: [new Date()]
+    }
+  }
+
+  addDate = date => {
+    this.setState(prevState => ({
+      datesWithMeals: [...prevState.datesWithMeals, date]
+    }))
+  }
+  
+  handleChange = value => {
+    this.addDate(value)
+  }
+  
   render () {
+    const styleCalendar = ({ date, view }) => {
+      if (view === 'month') {
+        if (this.state.datesWithMeals.find(el => el.toDateString() === date.toDateString())) {
+          return 'testClassName'
+        }
+      } else {
+        console.log('at least it ran')
+      }
+    }
+
     return (
       <div id='calView'>
         <NavBar />
-        <Calendar />
+        <Calendar onChange={this.handleChange} tileClassName={styleCalendar} />
       </div>
     )
   }
--- client/src/react-components/NavBar/index.js
+++ client/src/react-components/NavBar/index.js
@@ -10,16 +10,16 @@ export class NavBar extends Component {
   render () {
     const getRightSide = () => {
       if (!this.state.loggedIn) {
-        return ( <ul class="right"><li><a href='/signup'>Sign Up</a></li><li><a href='/login'>Log In</a></li></ul> )
+        return ( <ul className="right"><li><a href='/signup'>Sign Up</a></li><li><a href='/login'>Log In</a></li></ul> )
       } else {
-        return ( <ul class="right"><li><i class="material-icons left">person</i>Profile</li></ul> )
+        return ( <ul className="right"><li><i className="material-icons left">person</i>Profile</li></ul> )
       }
     }
 
     return (
-      <nav class="teal darken-3">
-        <div class='nav-wrapper'> 
-          <a href="." class="brand-logo">TEMP NAME</a>
+      <nav className="teal darken-3">
+        <div className='nav-wrapper'> 
+          <a href="." className="brand-logo">TEMP NAME</a>
           { getRightSide() }
         </div>
       </nav>
