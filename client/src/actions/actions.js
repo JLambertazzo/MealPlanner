4dd43d743210b07d6cb6db883adfdae475f1e281
Julien <jbertazzolambert@gmail.com>
2020-12-29 15:01:16 -0600

    actions made

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -17,7 +17,113 @@ export const createUser = (payload) => {
       log('error creating user')
     }
   }).then(json => {
-    log('Success: ', json)
+    log('Success:', json)
+    return json
+  }).catch(error => log(error))
+}
+
+export const login = (payload) => {
+  const request = new Request('/api/login', {
+    method: 'post',
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  return fetch(request).then(res => {
+    if (res.ok) {
+      return res.json()
+      // TODO change session user
+    } else {
+      log('error logging in')
+    }
+  }).then(json => {
+    log('Success:', json)
+    return json
+  }).catch(error => log(error))
+}
+
+export const addMeal = (payload, uid) => {
+  const request = new Request(`/api/users/${uid}/meals`, {
+    method: 'post',
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  return fetch(request).then(res => {
+    if (res.ok) {
+      return res.json()
+    } else {
+      log('error creating meal')
+    }
+  }).then(json => {
+    log('Success:', json)
+    return json
+  }).catch(error => log(error))
+}
+
+export const addIngredient = (payload, uid) => {
+  const request = new Request(`/api/users/${uid}/ingredients`, {
+    method: 'post',
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  return fetch(request).then(res => {
+    if (res.ok) {
+      return res.json()
+    } else {
+      log('error creating ingredient')
+    }
+  }).then(json => {
+    log('Success:', json)
+    return json
+  }).catch(error => log(error))
+}
+
+export const deleteMeal = (payload, uid, mid) => {
+  const request = new Request(`/api/users/${uid}/meals/${mid}`, {
+    method: 'delete',
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  return fetch(request).then(res => {
+    if (res.ok) {
+      return res.json()
+    } else {
+      log('error deleting meal')
+    }
+  }).then(json => {
+    log('Success:', json)
+    return json
+  }).catch(error => log(error))
+}
+
+export const deleteIngredient = (payload, uid, iid) => {
+  const request = new Request(`/api/users/${uid}/ingredients/${iid}`, {
+    method: 'delete',
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  return fetch(request).then(res => {
+    if (res.ok) {
+      return res.json()
+    } else {
+      log('error deleting ingredient')
+    }
+  }).then(json => {
+    log('Success:', json)
     return json
   }).catch(error => log(error))
 }
