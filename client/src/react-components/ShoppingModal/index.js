989d74d0acce8b1ee04b171e81e89cc84cbebb9e
Julien <jbertazzolambert@gmail.com>
2021-01-29 22:57:06 -0500

    YES! IMPLEMENTED UNITS!

--- client/src/react-components/ShoppingModal/index.js
+++ client/src/react-components/ShoppingModal/index.js
@@ -4,6 +4,7 @@ import { uid } from 'react-uid'
 import { getUserById } from '../../actions/actions'
 import { Button, Typography } from '@material-ui/core'
 import { ChevronLeft, FileCopy, Print, Email } from '@material-ui/icons'
+import convert from 'convert-units'
 import './styles.css'
 
 export class ShoppingModal extends Component {
@@ -20,19 +21,22 @@ export class ShoppingModal extends Component {
       user.meals.forEach(meal => {
         if (Date.now() < new Date(meal.date).getTime() || (new Date()).toDateString() === new Date(meal.date).toDateString()) {
           meal.ingredients.forEach(ingredient => {
-            if (need[ingredient.name]) {
-              need[ingredient.name] += ingredient.qty
+            let [qty, units] = this.convertUnits(ingredient.qty, ingredient.units)
+            if (!need[ingredient.name]) {
+              need[ingredient.name] = {}
+            }
+            if (!need[ingredient.name][units]) {
+              need[ingredient.name][units] = qty
             } else {
-              need[ingredient.name] = ingredient.qty
+              need[ingredient.name][units] += qty
             }
           })
         }
       })
       user.ingredients.forEach(ingredient => {
-        if (need[ingredient.name] && (need[ingredient.name] - ingredient.qty) > 0) {
-          need[ingredient.name] -= ingredient.qty
-        } else if (need[ingredient.name]) {
-          delete need[ingredient.name]
+        let [qty, units] = this.convertUnits(ingredient.qty, ingredient.units)
+        if (need[ingredient.name] && need[ingredient.name][units]) {
+          need[ingredient.name][units] -= qty
         }
       })
       this.setState({
@@ -41,13 +45,35 @@ export class ShoppingModal extends Component {
     })
   }
 
+  convertUnits = (qty, units) => {
+    if (['ml', 'l', 'tsp', 'Tbs', 'cup', 'pnt'].includes(units)) {
+      return [convert(qty).from(units).to('ml'), 'ml']
+    } else if (['kg', 'g', 'oz', 'lb'].includes(units)) {
+      return [convert(qty).from(units).to('kg'), 'kg']
+    } else {
+      console.log('Error converting')
+      return [qty, units]
+    }
+  }
+
   handleReturn = () => {
     this.props.exit()
   }
 
   getText = () => {
-    const text = Object.keys(this.state.need).reduce((acc, curr) => {
-      return acc = `${acc}- ${curr}: ${this.state.need[curr]}\n`
+    const text = Object.keys(this.state.need).reduce((acc, name) => {
+      let kgtext = null
+      let mltext = null
+      if (this.state.need[name]['kg']) {
+        let qty = this.state.need[name]['kg']
+        kgtext = `${name}: ${qty} kg${qty > 1 ? 's' : ''}\n`
+      }
+      if (this.state.need[name]['ml']) {
+        let qty = this.state.need[name]['ml']
+        mltext = `${name}: ${qty} mL${qty > 1 ? 's' : ''}\n`
+      }
+      let totalText = (kgtext ? kgtext : '') + (mltext ? mltext : '')
+      return acc + totalText
     }, '')
     return text
   }
@@ -58,6 +84,41 @@ export class ShoppingModal extends Component {
 
   handleEmail = () => {
     window.open(`mailto:?subject=Groceries&body=${this.getText()}`, '_blank')
+  }
+
+  getListElements = name => {
+    let kgtext = null
+    let mltext = null
+    if (this.state.need[name]['kg']) {
+      let qty = this.state.need[name]['kg']
+      kgtext = `${name}: ${qty} kg${qty > 1 ? 's' : ''}`
+    }
+    if (this.state.need[name]['ml']) {
+      let qty = this.state.need[name]['ml']
+      mltext = `${name}: ${qty} ml${qty > 1 ? 's' : ''}`
+    }
+    if (kgtext && mltext) {
+      return(
+        <div>
+          <li key={uid(name + 'kg')}>{kgtext}</li>
+          <li key={uid(name + 'ml')}>{mltext}</li>
+        </div>
+      )
+    } else if (kgtext) {
+      return(
+        <div>
+          <li key={uid(name + 'kg')}>{kgtext}</li>
+        </div>
+      )
+    } else if (mltext) {
+      return(
+        <div>
+          <li key={uid(name + 'ml')}>{mltext}</li>
+        </div>
+      )
+    } else {
+      return(<div></div>)
+    }
   }
 
   render() {
@@ -77,9 +138,9 @@ export class ShoppingModal extends Component {
         <div id="shoppingContainer">
           <ul>
             {
-              Object.keys(this.state.need).map(key => {
+              Object.keys(this.state.need).map(name => {
                 return(
-                  <li key={uid(key)}>{this.state.need[key]} {key}</li>
+                  this.getListElements(name)
                 )
               })
             }
