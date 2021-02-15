2e6fb5662d48de2133313ffc5614611e682223be
Julien <jbertazzolambert@gmail.com>
2021-02-15 10:31:12 -0500

    made calendar fullscreen

--- client/src/pages/calendar/CalendarView.js
+++ client/src/pages/calendar/CalendarView.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 import Calendar from 'react-calendar'
-import { Button, List, ListItem, ListItemText, Typography } from '@material-ui/core'
+import { Button, List, ListItem, ListItemText, Typography, Drawer } from '@material-ui/core'
 import { Edit } from '@material-ui/icons'
 import ListIcon from '@material-ui/icons/List'
 import { getUserById } from '../../actions/actions'
@@ -63,20 +63,18 @@ export default function CalendarView (props) {
       <NavBar uid={props.uid} />
       <div id='content'>
         <div id='main'>
-          <div>
-            <Calendar
-              className="custom-calendar-styles"
-              calendarType='US'
-              onChange={(value) => handleChange(value, setShowListModal, setSelectedDate)}
-              tileContent={calendarContent}
-              tileClassName={'calendar-button-styles'}
-              minDetail='month'
-            />
-          </div>
-          <div id="buttonContainer" className={(showListModal || showMealModal || showShoppingModal || showIngredientModal ? 'hide' : '')}>
+          <Calendar
+            className="custom-calendar-styles"
+            calendarType='US'
+            onChange={(value) => handleChange(value, setShowListModal, setSelectedDate)}
+            tileContent={calendarContent}
+            tileClassName={'calendar-button-styles'}
+            minDetail='month'
+          />
+          {/* <div id="buttonContainer" className={(showListModal || showMealModal || showShoppingModal || showIngredientModal ? 'hide' : '')}>
             <Button color='primary' variant='contained' onClick={() => setShowShoppingModal(true)} startIcon={<ListIcon />}>My Shopping List</Button>
             <Button color='primary' variant='contained' onClick={() => setShowIngredientModal(true)} startIcon={<Edit />}>Edit My Pantry</Button>
-          </div>
+          </div> */}
         </div>
         <ListModal
           uid={props.uid}
